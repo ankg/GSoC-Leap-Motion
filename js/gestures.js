7cbd4c91d7b4157fa889041cdd0f2bf5be4a5e03
ankg <aniketguptaknp@gmail.com>
2015-06-08 17:21:00 +0530

    Change js filename

---
+++ js/gestures.js
@@ -0,0 +1,20 @@
+var paused = true;
+var interactiveIframe = document.querySelector('.interactive');
+var interactive = new iframePhone.ParentEndpoint(interactiveIframe);
+
+Leap.loop({enableGestures: true}, function (frame) {
+
+if (frame.valid && frame.gestures.length > 0) {
+  frame.gestures.forEach(function (gesture) {
+
+    if((gesture.type == "keyTap" || gesture.type == "screenTap") && paused == true){ 
+      interactive.post('play');
+      paused = false;
+    }
+    else if((gesture.type == "keyTap" || gesture.type == "screenTap") && paused == false) {
+      interactive.post('stop');
+      paused = true;
+    }
+  });
+}
+});
