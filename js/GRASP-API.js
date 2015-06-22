9ea55c1fe92ad899a3a41f2bc1ea97144ea585d4
ankg <aniketguptaknp@gmail.com>
2015-06-23 01:39:22 +0530

    merges the initialize and leapInfo method

--- js/GRASP-API.js
+++ js/GRASP-API.js
@@ -4,9 +4,7 @@
 /* global $ */
 /* exported GRASP */
 
-var GRASP = {
-    leapInfo : function(parentDiv, options){
-      //desired structure of options needed to be sent by the developer
+//desired structure of options needed to be sent by the developer
       /*
          {
           palmVelocity : {
@@ -19,13 +17,55 @@ var GRASP = {
           }
          }
       */
+var GRASP = {
+    leapInfo : function(parentDiv, options, frame){
+      //pre-processing
+      //leftDetected will be set true if left hand has been detected
+      //rightDetected will be set true if right hand has been detected
+      var leftDetected, rightDetected;
+      if(frame.hands.length === 1)
+      {  
+        leftDetected = (frame.hands[0].type == "left");
+        rightDetected = (frame.hands[0].type == "right");
+      }
+      else
+      {  
+        leftDetected = true;
+        rightDetected = true;
+      }
+
+
+      //Creating the left and right hand div's if they don't already exist
+      //Fetching if they exist already in the leftDiv and rightDiv vars
+      var leftDiv, rightDiv, title_left, title_right;
+      if($('.left-hand').length === 0 && $('.right-hand').length === 0)
+      {
+        leftDiv = $('<div class="left-hand">');
+        rightDiv = $('<div class="right-hand">');
+        title_left = $('<h2>Left Hand Properties</h2>');
+        title_right = $('<h2>Right Hand Properties</h2>');
+        title_left.appendTo(leftDiv);
+        title_right.appendTo(rightDiv);
+        leftDiv.appendTo(parentDiv);
+        rightDiv.appendTo(parentDiv);
+      }
+      else
+      {
+        leftDiv = $('.left-hand');
+        rightDiv = $('.right-hand');
+      }
+
+      if(leftDetected === false)
+        leftDiv.innerHTML = "";
+      else if(rightDetected === false)
+        rightDiv.innerHTML = "";
+
+      //for every key in options, render the data
       for(var key in options){
         var hand = options[key].hand;
         var value = options[key].value;
-        
-        var leftDiv = $('.left-hand');
-        var rightDiv = $('.right-hand');
         var propertyDiv, propertyValue;
+
         if($('.'+hand+'-'+key).length === 0)
         {  
           propertyDiv = $('<h3 class="' + hand + '-' + key + '">');
@@ -41,7 +81,6 @@ var GRASP = {
           propertyValue = $('.'+hand+'-'+key+' .value');
 
         propertyValue[0].innerHTML = value[0] + "";
-        console.log(propertyValue);
 
         if(propertyDiv.has(propertyValue).length === 0)
           propertyDiv.append(propertyValue);
@@ -58,16 +97,6 @@ var GRASP = {
           rightDiv.append(propertyDiv);
         }
       }
-    },
-    initialize : function (parentDiv) {
-      var leftDiv = $('<div class="left-hand">');
-      var rightDiv = $('<div class="right-hand">');
-      var title_left = $('<h2>Left Hand Properties</h2>');
-      var title_right = $('<h2>Right Hand Properties</h2>');
-      title_left.appendTo(leftDiv);
-      title_right.appendTo(rightDiv);
-      leftDiv.appendTo(parentDiv);
-      rightDiv.appendTo(parentDiv);
     }
 
   };
